2d06cb7b31c415c0e5cdf4aee20c898d8b399ee9
kaylamaxwell <kaylamaxwell0@gmail.com>
2024-04-18 13:38:15 -0500

    First file upload

---
+++ television.py
@@ -0,0 +1,61 @@
+class Television:
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int = 0
+    MAX_CHANNEL: int = 3
+
+    def __init__(self) -> None:
+        self.__status: bool = False
+        self.__muted: bool = False
+        self.__volume: int = Television.MIN_VOLUME
+        self.__channel: int = Television.MIN_CHANNEL
+
+    def power(self) -> None:
+        """Toggle the power status of the television."""
+        self.__status = not self.__status
+        if not self.__status:
+            self.__muted = False
+
+    def mute(self) -> None:
+        """Toggle the mute status of the television."""
+        if self.__status:
+            self.__muted = not self.__muted
+
+    def channel_up(self) -> None:
+        """Increase the current channel by one."""
+        if self.__status:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+            else:
+                self.__channel = Television.MIN_CHANNEL
+
+    def channel_down(self) -> None:
+        """Decrease the current channel by one."""
+        if self.__status:
+            if self.__channel > Television.MIN_CHANNEL:
+                self.__channel -= 1
+            else:
+                self.__channel = Television.MAX_CHANNEL
+
+    def volume_up(self) -> None:
+        """Increase the volume by one level."""
+        if self.__status:
+            self.__muted = False
+            if self.__volume < Television.MAX_VOLUME:
+                self.__volume += 1
+
+    def volume_down(self) -> None:
+        """Decrease the volume by one level."""
+        if self.__status:
+            self.__muted = False
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
+
+
+    def __str__(self) -> str:
+        """Return a string representation of the television state."""
+        power_status: str = "True" if self.__status else "False"
+        if self.__muted:
+            return f"Power = {power_status}, Channel = {self.__channel}, Volume = {Television.MIN_VOLUME}"
+        else:
+            return f"Power = {power_status}, Channel = {self.__channel}, Volume = {self.__volume}"
