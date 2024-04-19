9126877d4394184a45b351ced8561ccc6545af13
kaylamaxwell <kaylamaxwell0@gmail.com>
2024-04-19 14:54:39 -0500

    replaced unittest with pytest

--- test_television.py
+++ test_television.py
@@ -34,21 +34,12 @@ class TestTelevision:
         assert not self.tv._Television__muted
 
     def test_channel_up_down(self):
-        self.tv.power()  # Ensure TV is powered on
+        self.tv.power()
         initial_channel = self.tv._Television__channel
-
-        # Test channel_up()
         self.tv.channel_up()
-        new_channel_after_up = self.tv._Television__channel
-        assert new_channel_after_up == initial_channel + 1 or new_channel_after_up == Television.MIN_CHANNEL
-
-        # Test channel_down()
+        assert self.tv._Television__channel == initial_channel + 1
         self.tv.channel_down()
-        new_channel_after_down = self.tv._Television__channel
-        assert new_channel_after_down == initial_channel - 1 or new_channel_after_down == Television.MAX_CHANNEL
-
-        # Assert that after channel_down(), channel returns to initial_channel
-        assert new_channel_after_down == initial_channel
+        assert self.tv._Television__channel == initial_channel
 
     def test_volume_up_down(self):
         self.tv.power()
